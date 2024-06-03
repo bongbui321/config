423f6facd2f9268d4f973a0415db1fa37552d2cd
bongbui321 <hoang.bui.vh@gmail.com>
2024-06-02 23:19:21 -0400

    resize keymap

--- nvim/lua/bongbui321/remap.lua
+++ nvim/lua/bongbui321/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- resize splits
+vim.keymap.set("n", "<M-,>", "<c-w>5<")
+vim.keymap.set("n", "<M-.>", "<c-w>5>")
+vim.keymap.set("n", "<M-t>", "<C-W>+")
+vim.keymap.set("n", "<M-s>", "<C-W>-")
+
