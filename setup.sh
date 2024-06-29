2fa3953019002c8d779ff3ed6a80511b55a443c8
bongbui321 <hoang.bui.vh@gmail.com>
2024-06-29 16:43:08 -0400

    cleanup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-ln -s $DIR/nvim $HOME/.config/nvim
-ln -s $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+ln -sf $DIR/nvim $HOME/.config/nvim
+ln -sf $DIR/tmux/.tmux.conf $HOME/.tmux.conf 
