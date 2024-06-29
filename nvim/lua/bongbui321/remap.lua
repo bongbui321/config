bae5995146b1c2ac12656013b9303c79aa2598a1
bongbui321 <hoang.bui.vh@gmail.com>
2024-06-28 23:12:42 -0400

    control c

--- nvim/lua/bongbui321/remap.lua
+++ nvim/lua/bongbui321/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<C-c>", "\"+y")
+
 -- resize splits
 vim.keymap.set("n", "<M-,>", "<c-w>5<")
 vim.keymap.set("n", "<M-.>", "<c-w>5>")
