60ab44180bfd6ac685ccd6c33c318d0b704c85b8
bongbui321 <hoang.bui.vh@gmail.com>
2024-08-03 11:35:01 -0400

    flags for symlink

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  link_flags="-sF"
+else
+  link_flags="-sf"
+fi
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-ln -sf $DIR/nvim $HOME/.config/nvim
-ln -sf $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+ln $link_flags $DIR/nvim $HOME/.config/nvim
+ln $link_flags $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+ln $link_flags $DIR/alacritty/ $HOME/.config/alacritty
 
 ./setup_zshrc.sh
