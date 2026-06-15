854826abceb1037512ef4da9576c608495e98049
bongbui321 <hoang.bui.vh@gmail.com>
2026-06-15 08:11:26 +0700

    rm clangd, remove illuminate, yank relative file

--- nvim/lua/bongbui321/remap.lua
+++ nvim/lua/bongbui321/remap.lua
@@ -9,5 +9,5 @@ vim.keymap.set("n", "<M-s>", "<C-W>-")
 
 -- yank relative path of current buffer to clipboard
 vim.keymap.set("n", "<leader>yr", function()
-  vim.fn.setreg("+", vim.fn.expand("%"))
+  vim.fn.setreg("+", vim.fn.expand("%:."))
 end, { desc = "Yank relative path to clipboard" })
