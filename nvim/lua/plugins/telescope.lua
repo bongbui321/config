b86493fc5ad5a818031704126a277035b4da89e9
bongbui321 <hoang.bui.vh@gmail.com>
2026-06-20 17:50:45 +0700

    fugitive, autopairs, telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,15 +5,15 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   keys = {
-    { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-    { '<C-p>', '<cmd>Telescope git_files<cr>', desc = 'Find git files' },
-    { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
-    {
-      '<leader>fl',
-      function()
-        require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })
-      end,
-      desc = 'Grep prompt',
-    },
+    { '<leader>f', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+    { '<leader>g', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
+    -- { '<C-p>', '<cmd>Telescope git_files<cr>', desc = 'Find git files' },
+    -- {
+    --   '<leader>fl',
+    --   function()
+    --     require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })
+    --   end,
+    --   desc = 'Grep prompt',
+    -- },
   },
 }
