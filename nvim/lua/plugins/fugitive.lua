e6d58918ce385b034e9530bd720c00d20ce58c40
bongbui321 <hoang.bui.vh@gmail.com>
2026-06-20 18:05:35 +0700

    fugitive split vertical

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
   "tpope/vim-fugitive",
+  config = function()
+    -- setup statusline bar to include branch name
+    vim.o.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P %{FugitiveStatusline()}"
+    vim.keymap.set("n", "<leader>gs", ":vertical rightbelow Git<CR>")
+  end
 }
