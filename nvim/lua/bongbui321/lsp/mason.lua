4a4c55ed8a064f3c8bdca8f624fd4cbd61cffb6b
bongbui321 <hoang.bui.vh@gmail.com>
2026-06-21 10:51:33 +0700

    rm

---
+++ nvim/lua/bongbui321/lsp/mason.lua
@@ -0,0 +1,31 @@
+local servers = {
+  --"lua_ls",
+  "pyright",
+  --"clangd",
+}
+
+require("mason").setup()
+require("mason-lspconfig").setup {}
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local opts = {
+  ensured_installed = servers
+}
+
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("bongbui321.lsp.handlers").on_attach,
+	}
+
+  local require_ok, conf_opts = pcall(require, "bongbui321.lsp.settings." .. server)
+  if require_ok then
+    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+  end
+
+	vim.lsp.config(server, opts)
+	vim.lsp.enable(server)
+end
